2aee869e90acfd5dd0213507542cafcf1f04afb2
Thiago Nascente <thiagonascenteborges@gmail.com>
2026-04-25 17:38:13 -0300

    versao 1.0
    É a primeira versão modificada pelo aluno, nela somente é possível executar localmente na mesma máquina. Ainda falta testar na máquina aws (service no aws e client localmente, e ambos no aws).

--- python/EmployeeClient.py
+++ python/EmployeeClient.py
@@ -7,29 +7,87 @@ import EmployeeService_pb2_grpc
 
 import const
 
+def prints(op, response, aux=0):
+    textos = [
+        [f'Employee\'s data by id {aux}: \n' + str(response)], #0
+        [f'Updated salary employee from id {aux}\n' + str(response)], #1
+        [f'Employee\'s salary from id {aux}: ' + str(response)], #2
+        ['Added new employee' + str(response)], #3
+        [f'Updated title employee id {aux} ' + str(response)], #4
+        [f'Deleted employee id {aux} ' + str(response)], #5
+        ['All employees: \n' + str(response)], #6
+        [f'All {aux}: \n{str(response)}'] #7
+    ]
+    print(textos[op][0])
+
 def run():
     with grpc.insecure_channel(const.IP+':'+const.PORT) as channel:
         stub = EmployeeService_pb2_grpc.EmployeeServiceStub(channel)
 
         # Query an employee's data
-        response = stub.GetEmployeeDataFromID(EmployeeService_pb2.EmployeeID(id=101))
-        print ('Employee\'s data: ' + str(response))
+        exemplo = 101
+        print(f'----------- Buscando o funcionário de id {exemplo} -------------')
+        response = stub.GetEmployeeDataFromID(EmployeeService_pb2.EmployeeID(id=exemplo))
+        prints(0, response, exemplo)
+        
+        # promovendo alguem
+        exemplo = 101
+        print(f'----------- Promovendo funcionário de id {exemplo} -------------')
+        response = stub.PromoteEmployee(EmployeeService_pb2.PromoteRequest(id=exemplo, increase=0.1))
+        prints(1, response, exemplo)
+
+        #buscando salario
+        exemplo = 101
+        print(f'----------- Buscando salário do funcionário de id {exemplo} -------------')
+        response = stub.GetSalaryByID(EmployeeService_pb2.EmployeeID(id=exemplo))
+        prints(2,response,exemplo)
 
         # Add a new employee
-        response = stub.CreateEmployee(EmployeeService_pb2.EmployeeData(id=301, name='Jose da Silva', title='Programmer'))
-        print ('Added new employee ' + response.status)
+        exemplo = 301
+        print(f'----------- Adicionando novo funcionario de id {exemplo} -------------')
+        response = stub.CreateEmployee(EmployeeService_pb2.EmployeeData(id=301, name='Jose da Silva', title='Programmer', salary=5000))
+        prints(3,response)
+        
+        # Promovendo um funcionario
+        exemplo = 301
+        print(f'----------- Promovendo funcionario de id {exemplo} -------------')
+        response = stub.PromoteEmployee(EmployeeService_pb2.PromoteRequest(id=exemplo, increase=0.1))
+        prints(1,response,exemplo)
 
-        # Change an employee's title
-        response = stub.UpdateEmployeeTitle(EmployeeService_pb2.EmployeeTitleUpdate(id=301, title='Senior Programmer'))
-        print ('Updated employee ' + response.status)
+        # Criando funcionarios
+        print(f'----------- Adicionando novos funcionarios de diversos id\'s -------------')
+        response = stub.CreateEmployee(EmployeeService_pb2.EmployeeData(id=401, name='Thiago', title='Programmer', salary=5000))
+        prints(3,response)
+        
+        response = stub.CreateEmployee(EmployeeService_pb2.EmployeeData(id=501, name='Ricardo', title='Programmer', salary=6000))
+        prints(3,response)
+        
+        response = stub.CreateEmployee(EmployeeService_pb2.EmployeeData(id=601, name='Lucas', title='Programmer', salary=7000))
+        prints(3,response)
 
-        # Delete an employee
-        response = stub.DeleteEmployee(EmployeeService_pb2.EmployeeID(id=201))
-        print ('Deleted employee ' + response.status)
+        # Trocando cargo de um funcionario
+        exemplo = 301
+        print(f'----------- Trocando cargo do funcionario de id {exemplo} -------------')
+        response = stub.UpdateEmployeeTitle(EmployeeService_pb2.EmployeeTitleUpdate(id=exemplo, title='Senior Programmer'))
+        prints(4, response, exemplo)
 
-        # List all employees
+        # Deletando um funcionario
+        exemplo = 201
+        print(f'----------- Deletando o funcionario de id {exemplo} -------------')
+        response = stub.DeleteEmployee(EmployeeService_pb2.EmployeeID(id=exemplo))
+        prints(5, response, exemplo)
+
+        # Listando todos os funcionarios
+        print(f'----------- Listando todos os funcionarios -------------')
         response = stub.ListAllEmployees(EmployeeService_pb2.EmptyMessage())
-        print ('All employees: ' + str(response))
+        prints(6,response)
+        
+        # Listando todos os funcionarios de determinado cargo
+        cargo = 'Programmer'
+        print(f'----------- Listando todos os {cargo} -------------')
+        response = stub.ListEmployeesByTitle(EmployeeService_pb2.TitleRequest(title=cargo))
+        prints(7, response, cargo)
+
 
 if __name__ == '__main__':
     logging.basicConfig()
--- python/EmployeeService.py
+++ python/EmployeeService.py
@@ -11,12 +11,14 @@ empDB=[
  {
  'id':101,
  'name':'Saravanan S',
- 'title':'Technical Leader'
+ 'title':'Technical Leader',
+ 'salary':20000
  },
  {
  'id':201,
  'name':'Rajkumar P',
- 'title':'Sr Software Engineer'
+ 'title':'Sr Software Engineer',
+ 'salary':10000
  }
  ]
 
@@ -26,14 +28,15 @@ class EmployeeServer(EmployeeService_pb2_grpc.EmployeeServiceServicer):
     dat = {
     'id':request.id,
     'name':request.name,
-    'title':request.title
+    'title':request.title,
+    'salary':request.salary
     }
     empDB.append(dat)
     return EmployeeService_pb2.StatusReply(status='OK')
 
   def GetEmployeeDataFromID(self, request, context):
     usr = [ emp for emp in empDB if (emp['id'] == request.id) ] 
-    return EmployeeService_pb2.EmployeeData(id=usr[0]['id'], name=usr[0]['name'], title=usr[0]['title'])
+    return EmployeeService_pb2.EmployeeData(id=usr[0]['id'], name=usr[0]['name'], title=usr[0]['title'], salary=usr[0]['salary'])
 
   def UpdateEmployeeTitle(self, request, context):
     usr = [ emp for emp in empDB if (emp['id'] == request.id) ]
@@ -51,10 +54,36 @@ class EmployeeServer(EmployeeService_pb2_grpc.EmployeeServiceServicer):
   def ListAllEmployees(self, request, context):
     list = EmployeeService_pb2.EmployeeDataList()
     for item in empDB:
-      emp_data = EmployeeService_pb2.EmployeeData(id=item['id'], name=item['name'], title=item['title']) 
+      emp_data = EmployeeService_pb2.EmployeeData(id=item['id'], name=item['name'], title=item['title'], salary=item['salary']) 
       list.employee_data.append(emp_data)
     return list
-
+  
+  # Novas funcionalidades
+  def GetSalaryByID(self, request, context):
+    usr = [ emp for emp in empDB if (emp['id'] == request.id) ] 
+    return EmployeeService_pb2.EmployeeSalary(salary=usr[0]['salary'])
+  
+  def ListEmployeesByTitle(self, request, context):
+    list = EmployeeService_pb2.EmployeeDataList()
+    cargo_buscado = request.title.lower()
+    for item in empDB:
+        # Filtra: só processa se o cargo bater com o pesquisado
+        if item['title'].lower() == cargo_buscado:
+            emp_data = EmployeeService_pb2.EmployeeData(
+                id=item['id'], 
+                name=item['name'], 
+                title=item['title'], 
+                salary=item['salary']
+            )
+            list.employee_data.append(emp_data)
+            
+    return list
+  
+  def PromoteEmployee(self, request, context):
+    usr = [ emp for emp in empDB if (emp['id'] == request.id) ]
+    usr[0]['salary'] = round(usr[0]['salary'] * (1 + request.increase), 2)
+    return EmployeeService_pb2.StatusReply(status='OK')
+  
 def serve():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     EmployeeService_pb2_grpc.add_EmployeeServiceServicer_to_server(EmployeeServer(), server)
@@ -64,5 +93,8 @@ def serve():
 
 
 if __name__ == '__main__':
-    logging.basicConfig()
-    serve()
+    try:
+      logging.basicConfig()
+      serve()
+    except KeyboardInterrupt:
+      print('Serviço finalizado (ctrl + c)')
